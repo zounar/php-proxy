da9b65e30f1dcb70752d360e2304d29049119979
Robin Zounar <robin.zounar@gmail.com>
2020-06-07 21:17:56 +0200

    Reformatted code

--- proxy.php
+++ proxy.php
@@ -58,12 +58,9 @@ define('HR', PHP_EOL . PHP_EOL . '----------------------------------------------
  */
 function ri(&$variable, $default = null)
 {
-    if (isset($variable))
-    {
+    if (isset($variable)) {
         return $variable;
-    }
-    else
-    {
+    } else {
         return $default;
     }
 }
@@ -89,8 +86,7 @@ function getSkippedHeaders()
 }
 
 
-if (!function_exists('errorHandler'))
-{
+if (!function_exists('errorHandler')) {
     /**
      * @param int $code
      * @param string $message
@@ -104,14 +100,13 @@ if (!function_exists('errorHandler'))
 }
 
 
-if (!function_exists('exceptionHandler'))
-{
+if (!function_exists('exceptionHandler')) {
     /**
      * @param Exception $ex
      */
     function exceptionHandler(Exception $ex)
     {
-        exitWithError($ex->getMessage() . ' in '. $ex->getFile() . ' at line ' . $ex->getLine());
+        exitWithError($ex->getMessage() . ' in ' . $ex->getFile() . ' at line ' . $ex->getLine());
     }
 }
 
@@ -120,22 +115,17 @@ if (!function_exists('exceptionHandler'))
 
 // Compatibility checks
 
-if (!IGNORE_SAFE_MODE && function_exists('ini_get') && ini_get('safe_mode'))
-{
+if (!IGNORE_SAFE_MODE && function_exists('ini_get') && ini_get('safe_mode')) {
     exitWithError('Safe mode is enabled, this may cause problems with uploading files');
 }
 
-if (!function_exists('curl_init'))
-{
+if (!function_exists('curl_init')) {
     exitWithError('libcurl is not installed on this server');
 }
 
-if (class_exists('CURLFile'))
-{
+if (class_exists('CURLFile')) {
     define('CURLFILE', true);
-}
-else
-{
+} else {
     define('CURLFILE', false);
 }
 
@@ -147,44 +137,34 @@ set_exception_handler('exceptionHandler');
 //----------------------------------------------------------------------------------
 
 // Check for auth token
-if (ri($_SERVER[HTTP_PROXY_AUTH]) !== AUTH_KEY)
-{
+if (ri($_SERVER[HTTP_PROXY_AUTH]) !== AUTH_KEY) {
     exitWithError(HTTP_PROXY_AUTH . ' header is invalid');
 }
 
 // Check for debug token
-if (!empty($_SERVER[HTTP_PROXY_DEBUG]))
-{
+if (!empty($_SERVER[HTTP_PROXY_DEBUG])) {
     $debug = true;
-}
-else
-{
+} else {
     $debug = false;
 }
 
 // Get target URL
 $targetURL = ri($_SERVER[HTTP_PROXY_TARGET_URL]);
-if (empty($targetURL))
-{
-    exitWithError(HTTP_PROXY_TARGET_URL .' header is empty');
+if (empty($targetURL)) {
+    exitWithError(HTTP_PROXY_TARGET_URL . ' header is empty');
 }
-if (filter_var($targetURL, FILTER_VALIDATE_URL) === FALSE)
-{
-    exitWithError(HTTP_PROXY_TARGET_URL .' "'.$targetURL.'" is invalid');
+if (filter_var($targetURL, FILTER_VALIDATE_URL) === false) {
+    exitWithError(HTTP_PROXY_TARGET_URL . ' "' . $targetURL . '" is invalid');
 }
 
 //--------------------------------
 
 // Add GET params to target URL
-if (!empty($_SERVER['QUERY_STRING']))
-{
+if (!empty($_SERVER['QUERY_STRING'])) {
     $targetURLParts = parse_url($targetURL);
-    if (!empty($targetURLParts['query']))
-    {
+    if (!empty($targetURLParts['query'])) {
         $targetURL = $targetURL . '&' . $_SERVER['QUERY_STRING'];
-    }
-    else
-    {
+    } else {
         $targetURL = trim($targetURL, '\?');
         $targetURL = $targetURL . '?' . $_SERVER['QUERY_STRING'];
     }
@@ -199,32 +179,23 @@ $request = curl_init($targetURL);
 
 // Set input data
 $requestMethod = strtoupper(ri($_SERVER['REQUEST_METHOD']));
-if ($requestMethod === "PUT" || $requestMethod === "PATCH")
-{
+if ($requestMethod === "PUT" || $requestMethod === "PATCH") {
     curl_setopt($request, CURLOPT_POSTFIELDS, file_get_contents('php://input'));
-}
-elseif ($requestMethod === "POST")
-{
+} elseif ($requestMethod === "POST") {
     $data = array();
 
-    if (!empty($_FILES))
-    {
-        if (!CURLFILE)
-        {
+    if (!empty($_FILES)) {
+        if (!CURLFILE) {
             curl_setopt($request, CURLOPT_SAFE_UPLOAD, false);
         }
 
-        foreach ($_FILES as $fileName => $file)
-        {
+        foreach ($_FILES as $fileName => $file) {
             $filePath = realpath($file['tmp_name']);
 
-            if (CURLFILE)
-            {
+            if (CURLFILE) {
                 $data[$fileName] = new CURLFile($filePath);
-            }
-            else
-            {
-                $data[$fileName] = '@'.$filePath;
+            } else {
+                $data[$fileName] = '@' . $filePath;
             }
         }
     }
@@ -237,14 +208,15 @@ elseif ($requestMethod === "POST")
 // Parse request headers
 $httpHeaders = array();
 $httpHeadersAll = array();
-foreach ($_SERVER as $key => $value)
-{
-    if (strpos($key, 'HTTP_') === 0)
-    {
-        $header = str_replace('_', '-', ucwords(strtolower(str_replace('HTTP_', '', $key)), '_')) . ': '. $value;
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $header = str_replace(
+                '_',
+                '-',
+                ucwords(strtolower(str_replace('HTTP_', '', $key)), '_')
+            ) . ': ' . $value;
 
-        if (!in_array($key, getSkippedHeaders()))
-        {
+        if (!in_array($key, getSkippedHeaders())) {
             $httpHeaders[] = $header;
         }
 
@@ -270,18 +242,17 @@ $responseHeader = substr($response, 0, $headerSize);
 $responseBody = substr($response, $headerSize);
 $responseInfo = curl_getinfo($request);
 $responseCode = ri($responseInfo['http_code'], 500);
-$requestHeaders = preg_split( '/[\r\n]+/', ri($responseInfo['request_header'], ''));
-if ($responseCode == 0)
-{
+$requestHeaders = preg_split('/[\r\n]+/', ri($responseInfo['request_header'], ''));
+if ($responseCode == 0) {
     $responseCode = 404;
 }
 
 // Get real target URL after all redirects
 $finalRequestURL = curl_getinfo($request, CURLINFO_EFFECTIVE_URL);
-if (!empty($finalRequestURL))
-{
+if (!empty($finalRequestURL)) {
     $finalRequestURLParts = parse_url($finalRequestURL);
-    $finalURL = ri($finalRequestURLParts['scheme'], 'http') . '//' . ri($finalRequestURLParts['host']) . ri($finalRequestURLParts['path'], '');
+    $finalURL = ri($finalRequestURLParts['scheme'], 'http') . '//' .
+        ri($finalRequestURLParts['host']) . ri($finalRequestURLParts['path'], '');
 }
 
 curl_close($request);
@@ -291,31 +262,24 @@ curl_close($request);
 // Split header text into an array.
 $responseHeaders = preg_split('/[\r\n]+/', $responseHeader);
 // Pass headers to output
-foreach ($responseHeaders as $header)
-{
+foreach ($responseHeaders as $header) {
     // Pass following headers to response
-    if (preg_match('/^(?:Content-Type|Content-Language|Content-Security|X)/i', $header))
-    {
+    if (preg_match('/^(?:Content-Type|Content-Language|Content-Security|X)/i', $header)) {
         header($header);
-    }
-    // Replace cookie domain and path
-    elseif (strpos($header, 'Set-Cookie') !== false)
-    {
+    } // Replace cookie domain and path
+    elseif (strpos($header, 'Set-Cookie') !== false) {
         $header = preg_replace('/((?>domain)\s*=\s*)[^;\s]+/', '\1.' . $_SERVER['HTTP_HOST'], $header);
         $header = preg_replace('/\s*;?\s*path\s*=\s*[^;\s]+/', '', $header);
         header($header, false);
-    }
-    // Decode response body if gzip encoding is used
-    elseif ($header === 'Content-Encoding: gzip')
-    {
+    } // Decode response body if gzip encoding is used
+    elseif ($header === 'Content-Encoding: gzip') {
         $responseBody = gzdecode($responseBody);
     }
 }
 
 //----------------------------------
 
-if ($debug)
-{
+if ($debug) {
     echo 'Headers sent to proxy' . PHP_EOL . PHP_EOL;
     echo implode($httpHeadersAll, PHP_EOL);
     echo HR;
@@ -342,9 +306,7 @@ if ($debug)
 
     echo 'Body sent from proxy to client' . PHP_EOL . PHP_EOL;
     echo $responseBody;
-}
-else
-{
+} else {
     http_response_code($responseCode);
     exit($responseBody);
 }
